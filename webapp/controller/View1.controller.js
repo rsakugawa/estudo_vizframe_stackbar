7a0bb903ed3ae97d8269c2c1f3101fccbf1209c9
rsakugawa <rafael.sakugawa@yahoo.com.br>
2021-03-21 10:23:25 -0300

    Primeiro Commmit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,22 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+],
+	/**
+	 * @param {typeof sap.ui.core.mvc.Controller} Controller
+	 */
+	function (Controller) {
+		"use strict";
+
+		return Controller.extend("estudovizframestackedbar.controller.View1", {
+			onInit: function () {
+
+                var jsonData = new sap.ui.model.json.JSONModel("model/Data.json");
+                var oVizFrame = this.getView().byId("idVizFrame");
+                oVizFrame.setModel(jsonData);
+
+               var oVizFrameDataForTable = this.getView().byId("chartContainerContentTable");
+                oVizFrameDataForTable.setModel(jsonData);
+
+			}
+		});
+	});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"estudo_vizframe_stackedbar/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
